b611e61022643a48b0cdefc7e2cd365e5c3293ce
tmctanks <tmc12344@gmail.com>
2023-04-02 14:16:43 -0600

    added all operator buttons and equal button functionality, calculator is fully functional

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttons = document.getElementById('buttons');
-const num1 = document.getElementById('num1');
-const totalbox = document.getElementById('total');
+const num1Box = document.getElementById('num1');
+const totalBox = document.getElementById('total');
 const equalsBut = document.getElementById('enter');
 const addBut = document.getElementById('add');
 const subtractBut = document.getElementById('subtract');
@@ -8,9 +8,11 @@ const multiplyBut = document.getElementById('multiply');
 const divideBut = document.getElementById('divide');
 
 var op = null; 
+var num2 = 0;
 
 buttonFunction();
 
+/*All mathmatical operations*/
 function add(a,b){
     return (a + b);
 }
@@ -32,17 +34,50 @@ function buttonFunction(){
     for(i = 0; i < 10;i++){
         const button = document.getElementById(i);
         button.addEventListener("mouseup" , function(){
-            num1.textContent = num1.textContent + button.innerHTML;
+            num1Box.textContent = num1Box.textContent + button.innerHTML;
             console.log(button.innerHTML);
         })
     }
-    addBut.addEventListener("mouseup" , () => op = "+")
-    subtractBut.addEventListener("mouseup" , () => op = "-")
-    addBut.addEventListener("mouseup" , () => op = "+")
-    addBut.addEventListener("mouseup" , () => op = "+")
-    equalsBut.addEventListener("mouseup" , () => console.log(op));
+    addBut.addEventListener("mouseup", function(){
+        op = "+"
+        totalBox.textContent = totalBox.textContent + ' ' + num1Box.textContent + ' ' + op;
+        num2 = num1Box.textContent
+        num1Box.textContent = ""
+    })
+    subtractBut.addEventListener("mouseup", function(){
+        op = "-"
+        totalBox.textContent = totalBox.textContent + ' ' + num1Box.textContent + ' ' + op;
+        num2 = num1Box.textContent
+        num1Box.textContent = ""
+    })
+    multiplyBut.addEventListener("mouseup", function(){
+        op = "*"
+        totalBox.textContent = totalBox.textContent + ' ' + num1Box.textContent + ' ' + op;
+        num2 = num1Box.textContent
+        num1Box.textContent = ""
+    })
+    divideBut.addEventListener("mouseup", function(){
+        op = "/"
+        totalBox.textContent = totalBox.textContent + ' ' + num1Box.textContent + ' ' + op;
+        num2 = num1Box.textContent
+        num1Box.textContent = ""
+    })
+    equalsBut.addEventListener("mouseup" , () => num1Box.textContent = operate(num2, num1Box.textContent,op));
 }
 
+/* Chooses which operation to use and returns it*/
 function operate(a,b,op){
-
+    a = parseInt(a);
+    b = parseInt(b);
+    if(op == "+"){
+        return add(a,b);
+    }else if(op =="-"){
+        return subtract(a,b);
+    }else if(op == "*"){
+        return multiply(a,b);
+    }else if (op == "/"){
+        return divide(a,b);
+    }else{
+        return null;
+    }
 }
